083da39705a33900696b951b7b62b71adf2efb53
Rolf Pedersen <rolfhsp@gmail.com>
2021-09-12 22:19:43 +0200

    Initial commit.

---
+++ src/main/java/com/example/jsonviewdemo/JsonviewdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonviewdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonviewdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonviewdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jsonviewdemo/JsonviewdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonviewdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonviewdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
