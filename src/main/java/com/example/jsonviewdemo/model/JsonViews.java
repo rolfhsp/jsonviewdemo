b9a783bde1b4eb6a82dd1234d3e2832dab53a674
Rolf Pedersen <rolf.pedersen@spk.no>
2021-09-15 02:00:40 +0200

    Define views for Agent data.

---
+++ src/main/java/com/example/jsonviewdemo/model/JsonViews.java
@@ -0,0 +1,7 @@
+package com.example.jsonviewdemo.model;
+
+public interface JsonViews {
+    public interface Basic {}
+    public interface QView extends Basic {}
+    public interface MView extends Basic {}
+}
