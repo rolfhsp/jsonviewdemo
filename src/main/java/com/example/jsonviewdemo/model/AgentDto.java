fc116e61238ffe5845380c1bd6117e767a231ff8
Rolf Pedersen <rolf.pedersen@spk.no>
2021-09-15 02:00:40 +0200

    Define views for Agent data.

--- src/main/java/com/example/jsonviewdemo/model/AgentDto.java
+++ src/main/java/com/example/jsonviewdemo/model/AgentDto.java
@@ -2,18 +2,35 @@ package com.example.jsonviewdemo.model;
 
 
 import com.example.jsonviewdemo.api.ApiAgent;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
 @Data @AllArgsConstructor
 public class AgentDto implements ApiAgent {
+
+    @JsonView(JsonViews.Public.class)
     private Long id;
+
+    @JsonView({JsonViews.MView.class, JsonViews.QView.class})
     private String codeName;
+
+    @JsonView(JsonViews.MView.class)
     private String realName;
+
+    @JsonView(JsonViews.MView.class)
     private Boolean licenseToKill;
+
+    @JsonView(JsonViews.QView.class)
     private String vehicle;
+
+    @JsonView(JsonViews.QView.class)
     private String weapon;
+
+    @JsonView(JsonViews.MView.class)
     private String phrase;
+
+    @JsonView(JsonViews.MView.class)
     private String mission;
 }
---
+++ src/main/java/com/example/jsonviewdemo/model/JsonViews.java
@@ -0,0 +1,7 @@
+package com.example.jsonviewdemo.model;
+
+public interface JsonViews {
+    public interface Public {}
+    public interface QView extends Public {}
+    public interface MView extends Public {}
+}
